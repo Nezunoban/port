f31741e8da6d9037da62ed335561c8cb414eab64
Nezunoban <19734799+Nezunoban@users.noreply.github.com>
2019-10-07 09:51:07 -0700

    New 2019 (#1)
    * HTML adjustments
    
    * Links added
    
    * update for js start for scroll
    
    * nav class added
    
    * Logo and theme folder
    
    * Init watching fix
    
    * Reboot.scss implemented with additional dependencies (hover.scss and spacing variables per
    
    * Color Integration, navigation initial styles
    
    * Mobile Navigation, angles, partial font
    
    * @font face font rendering issues chrome fixed, but still need work on loading class
    
    * Background angles with isometric gridlines and gradient and star img
    
    * Starting Isometric Portfolio Grid
    
    * fixing font errors (folder needed for "fonts/muli..." and init.css extra reference
    
    * Failed angled portfolio due to JS conflicts with preserve 3D
    
    * Removed float grid, removed transitions
    Added mobile navigation menu via pure drawer and styled and customized
    removed gradient shimmer buttons
    
    * Remove CSS Grid, mobile nav fixes with Pure Drawer, Frontpage layout fixes for mobile & tablet
    
    * Self intro padding
    
    * Work, Story individual page layouts with zurb grid
    
    * Addition of Self page, standardizing angle quotes and footer interaction
    
    * adjustments for self blue angles and improvements to portfolio with blue images, CBC progress page.
    
    * CBC story page, footer adjustment for final story to be beneath footer angle.
    
    * Adding contact page, unstyled
    
    * Adding error page
    
    * removed --> on error page
    
    * cursor pointer fixes
    
    * animsition links and page transitions
    
    * Slider alpha initiated.
    
    * padding fix on slide
    
    * Added shooting stars
    
    * Tablet fixes, Font Size fixes, Ipad adjustments, swappin granimate for coolBeans
    
    * Cool beans, Form Fixes, quotes changes, grid fixes
    
    * Scroll Mouse buttons added for down and up, size increase, menu flap fixing, quote slider spacing adjustments, removing negative translate for horizontal mobile devices
    
    * Fixed font sizes for work page to have 3 columns and 20vw on slider top margin
    
    * Adding #main
    
    * Changing main anchor to hero for scroll up
    
    * hero anchor
    
    * hero anchor for mouse scroller
    
    * Logo changes colors on scroll on all but error and contact pages
    
    * drawer hamburger animation fix for safari
    
    * Added intro animations for anglegrid__items and fixed them to be off canvas initially
    
    * Added story full website design modal, adjusted angled portfolio to align with 34deg iso tilt.
    
    * Icon page fixes, svg icons for self and work sections
    
    * fixed padding left for 1700px to 1900px
    
    * added back minimum buttons
    
    * Added SecFed page, removed useless modal buttons from story pages, fixed non-clickable slider link, fixed button colors for modal, added clickable class to all images for angled portfolio, coolbeans fontweight fix.
    
    * added admin and msde fixes to headers for readability
    
    * adding all but laboure story pages
    
    * immage change for tqi
    
    * fix hero input color
    
    * Hero text area color fix
    
    * adding back mouse scroll trigger on work.html to prevent overlap
    
    * adding form to contact form via Amazon Congito and SNS
    
    * fix for divs
    
    * removing options for submitting for now
    
    * welcome text
    
    * self text
    
    * spelling self
    
    * adding button for work
    
    * add button self page
    
    * formspree.io implementation
    
    * formspree
    
    * form style update
    
    * testcss
    
    * removing olld contact
    
    * Load fixes and corrections
    
    * Adding quicksand fonts, fixing inline icon buttons, heading fixes
    
    * add back in fade ups for aos, self page alignment for volunteering
    
    * fixing fonts, paper icons, etc.
    
    * adding _blank to pdf links
    
    * added nichols quote, fixed slider margin top for large screens
    
    * Javascript fixes for load issues and clicking slider on mobile
    
    * Removing animsition from quotes, fixing pdf icon styling, timeout on ani
    
    * Fixing self linking overflow issue and reinitiating all aos and animsist
    
    * Fixes for overlap for iOS Safari
    
    * Page spelling Fixes, slider fix
    
    * remove quicksand

---
+++ js/scroll.js
@@ -0,0 +1,27 @@
+
+
+
+
+$(window).scroll(function () {
+    var sc = $(window).scrollTop()
+    if (sc > 100) {
+        $("#header-sroll").addClass("small")
+    } else {
+        $("#header-sroll").removeClass("small")
+    }
+});
+
+
+/*
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+       $('nav').addClass('fixed-header');
+    }
+    else {
+       $('nav').removeClass('fixed-header');
+    }
+});
+*/
+/* scrollTop() >= 240
+   Should be equal the the height of the header
+ */
---
+++ js/scrollhead.js
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2017 by kaemak (https://codepen.io/kaemak/pen/mHyKa)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
